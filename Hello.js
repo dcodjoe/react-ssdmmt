75cc6158d3e8633529f54ca05d7eb0840fcd6191
dcodjoe <dcodjoe@hotmail.com>
2020-03-22 19:05:34 -0700

    Initial commit

---
+++ Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import download from './download.png';
+
+export default ({ name }) => <h1> {name} </h1>
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ssdmmt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ssdmmt)
